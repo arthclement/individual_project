9d7b0da3a86eb8b9b1d051e16af272c17130c326
arthclement <arthclems@gmail.com>
2018-03-13 11:39:52 +0100

    login.php working

--- public/login.php
+++ public/login.php
@@ -6,3 +6,41 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $passwordCheck = false;
 $usernameCheck = false;
+    
+try {
+    $connection = Service\DBConnector::getConnection();
+} catch (RuntimeException $e) {
+    http_response_code(500);
+    echo 'An error occured, please contact support';
+    exit(1);
+}
+
+$sqlSearchForUser = "SELECT * FROM register.user WHERE username=\"$username\" AND password=\"$password\"";
+
+$sqlQuery = $connection->prepare($sqlSearchForUser);
+$sqlQuery->execute();
+$usernameReturnDB = $sqlQuery->fetchAll();
+
+if (!$usernameReturnDB) {
+    echo 'Incorrect username or password';
+    return;
+}
+
+session_start();
+$_SESSION['username'] = $username;
+$_SESSION['user_id'] = $usernameReturnDB[0]['iduser'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login page</title>
+</head>
+<body>
+    <h1>You're successfully logged in!</h1>
+    <p>Welcome <?php echo $_SESSION['username']; ?>!</p>
+    <p>Your userID is <?php echo $_SESSION['user_id']; ?></p>
+</body>
+</html>
